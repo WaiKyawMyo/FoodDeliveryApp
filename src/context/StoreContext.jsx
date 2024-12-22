5e8092d35b3f6ccde0d78ef1a184a40aa1a3b3e0
Wai Kyaw Myo <waikyawmyo4u@gmail.com>
2024-12-22 23:55:13 +0700

    Day 2 finish

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,12 +1,31 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props)=>{
-  const contextvalue ={
-      food_list
+  const [cartItem,setcartItem]=useState({})
+
+  const addToCart=(itemID)=>{
+    if(!cartItem[itemID]){
+      setcartItem((prev)=>({...prev,[itemID]:1}))
+    }else{
+      setcartItem((prev)=>({...prev,[itemID]:prev[itemID]+1}))
+    }
   }
+  const removeFromCard =(itemID)=>{
+    setcartItem((prev)=>({...prev,[itemID]:prev[itemID]-1}))
+  }
+  const contextvalue ={
+      food_list,
+      cartItem,
+      setcartItem,
+      addToCart,
+      removeFromCard
+  }
+  useEffect(()=>{
+     console.log(cartItem)
+  },[cartItem])
   return(
     <StoreContext.Provider value={contextvalue}>
       {props.children}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import '../Home/Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
 
 
 const Home = () => {
@@ -10,7 +12,8 @@ const Home = () => {
     <div>
         <Header/>
         <ExploreMenu category={category} setcategory={setcategory}/>
-       
+       <FoodDisplay category={category}/>
+       <AppDownload />
     </div>
   )
 }
